42e15a24e0ced0dbdcf2ddf0859a5f070f57425c
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-02-28 16:04:44 +0900

    refactored. added comment

--- learning_wikipedia_api/get_name_in_kana_from_wikidata.py
+++ learning_wikipedia_api/get_name_in_kana_from_wikidata.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 get name in kana, and date of birth from https://www.wikidata.org
-
 I used Wikidata API.
 
 Wikibase/API - MediaWiki
@@ -12,7 +11,6 @@ https://www.wikidata.org/wiki/Q17686476
 
 For example:
 https://www.wikidata.org/w/api.php?action=wbgetentities&sites=jawiki&titles=久間田琳加&languages=ja
-
 """
 
 import requests
@@ -37,13 +35,15 @@ for id in data['entities'].keys():
     yomi = ""
     birth = ""
     
+    kana_ID = "P1814" # https://www.wikidata.org/wiki/Property:P1814
+    date_of_birth_ID = "P569" # https://www.wikidata.org/wiki/Property:P569
+    
     try:
         name = data['entities'][id]["labels"]['ja']['value']
-        yomi = data['entities'][id]["claims"]["P1814"][0]["mainsnak"]["datavalue"]["value"]
-        birth = data['entities'][id]["claims"]["P569"][0]["mainsnak"]["datavalue"]["value"]["time"]
+        yomi = data['entities'][id]["claims"][kana_ID][0]["mainsnak"]["datavalue"]["value"]
+        birth = data['entities'][id]["claims"][date_of_birth_ID][0]["mainsnak"]["datavalue"]["value"]["time"]
         birth = birth[1:11] # yyyy-mm-dd
     except:
         pass
         
     print(name + "," + yomi + "," + birth)
-
