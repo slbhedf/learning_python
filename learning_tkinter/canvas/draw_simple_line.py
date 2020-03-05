f6aa5d08decc0ee196680409bc2ebc778ecc6362
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-03-05 18:08:40 +0900

    edited comment

--- learning_tkinter/canvas/draw_simple_line.py
+++ learning_tkinter/canvas/draw_simple_line.py
@@ -7,7 +7,7 @@ canvas = Canvas (top, bg="pink")
 canvas['width'] = 300
 canvas['height'] = 300
 
-# draw a line from left top to right bottom
+# draw a line from top left to bottom right
 coord_line = 0, 0, 300, 300
 line = canvas.create_line(coord_line, fill="black")
 
