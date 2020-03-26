5b07543c572a53bacb4b63138a0bf05d040d5a78
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-03-26 18:42:14 +0900

    add ylabels, and save a figure as a svg file

--- learning_matplotlib/plot_cosx.py
+++ learning_matplotlib/plot_cosx.py
@@ -3,7 +3,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = np.arange(0,2*np.pi, 0.05)
+x = np.arange(0, np.pi, 0.05)
 y = np.cos(x)
 fig, ax = plt.subplots()  
 ax.plot(x, y)  
@@ -16,9 +16,15 @@ fig.set_size_inches(10, 10)
 
 ax.grid(axis='both')
 
-xaxis_ticks=[np.pi/6.0, np.pi/4.0, np.pi/3.0, np.pi/2.0]
-xlabels = ['pi/6', 'pi/4' , 'pi/3' ,'pi/2']
+xaxis_ticks=[0, np.pi/6.0, np.pi/4.0, np.pi/3.0, np.pi/2.0, 2*np.pi/3.0, 3*np.pi/4.0, 5*np.pi/6.0, np.pi]
+xlabels = ['0', 'π/6', 'π/4' , 'π/3' ,'π/2', '2π/3', '3π/4', '5π/6', 'π']
 plt.xticks(ticks=xaxis_ticks, labels=xlabels, rotation=30)
+
+yaxis_ticks=list(map(np.cos, xaxis_ticks))
+ylabels = ['1','√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', '-√3/2', '-1']
+plt.yticks(ticks=yaxis_ticks, labels=ylabels, rotation=0)
+
+plt.savefig('cosx_20200326.svg')
 
 plt.show()
 
