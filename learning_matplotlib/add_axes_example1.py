ff7581f8169f74d6cefa278d4eab1e72139a9e20
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-03-27 16:57:10 +0900

    Update add_axes_example1.py

--- learning_matplotlib/add_axes_example1.py
+++ learning_matplotlib/add_axes_example1.py
@@ -17,8 +17,14 @@ fig = plt.figure()
 ax1 = fig.add_axes([left1, bottom1, width1, height1])
 ax2 = fig.add_axes([left2, bottom2, width2, height2])
 
-ax1.plot(np.arange(0,10), np.ones(10))
-ax2.plot(np.arange(0,10), np.arange(0,10))
+ax1.plot(np.arange(0,10), np.ones(10), label='y=1')
+ax2.plot(np.arange(0,10), np.arange(0,10), label='y=x')
 
 ax1.set_title('y=1')
 ax2.set_title('y=x')
+
+ax1.legend(loc='best')
+ax2.legend(loc='upper left')
+
+ax1.set_ylim(0, 10)
+ax2.set_ylim(0, 10)
