650163be1025658fcf06f768789840c7c8a4be06
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-04-09 11:27:01 +0900

    Update plot_deaths_caused_by_coronavirus.py

--- learning_matplotlib/plot_deaths_caused_by_coronavirus.py
+++ learning_matplotlib/plot_deaths_caused_by_coronavirus.py
@@ -5,19 +5,31 @@ from datetime import timedelta, datetime
 import matplotlib.pyplot as plt
 
 def set_datelabels(start, end, ax):
-    xticks = []
+    xmajor_ticks = []
+    xminor_ticks = []
     xlabels = []
     i = 0
     d = start
     while(d <= end):
-        xticks.append(i)
-        xlabels.append(d.isoformat()[:10])
+        if i % 7 == 0:
+            xmajor_ticks.append(i)
+            xlabels.append(d.isoformat()[:10])
+        else:
+            xminor_ticks.append(i)
+            
         d = d + timedelta(days=1)
         i += 1
-    ax.set_xticks(ticks=xticks)
+        
+    ax.set_xticks(xmajor_ticks, minor=False)
+    ax.set_xticks(xminor_ticks, minor=True)
     ax.set_xticklabels(xlabels)
-    ax.grid(True)
+    
+    ax.grid(True, which='both')
+    ax.tick_params(which='major', length=10)
     plt.xticks(rotation=70)
+
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(20) 
 
 def dates_to_ints(dates, ax):
     date_string = ax.get_xticklabels()[0].get_text()
@@ -46,17 +58,21 @@ def set_logscale_yticks(ax):
     ax.set_yticklabels(yticklabels)
 
 ################## 
-fig = plt.figure(figsize=(20,20))
+fig = plt.figure(figsize=(25, 25))
 ax = fig.subplots()
-set_datelabels(datetime(2020,1,10), datetime(2020,4,8), ax)
+set_datelabels(datetime(2020,1,10), datetime(2020,4,7), ax)
 ax.set_yscale('log')
+ax.set_ylabel('total deaths', fontsize='20')
 set_logscale_yticks(ax)
 ax.set_ylim(0.90, 20000)
-#ax.set_xlim()
+
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(20) 
 
 df_all = pd.read_csv('full_data.csv', parse_dates=['date'])
 
-countries = ['Italy', 'Spain','United Kingdom', 'Germany', 'France', 'United States', 'China', 'Japan', 'Iran', 'Netherlands', 'Belgium']
+countries = ['Italy', 'Spain','United Kingdom', 'Germany', 'France', 'United States', 'China', 'Iran']
+#countries = ['Italy', 'Spain','United Kingdom', 'Germany', 'France', 'United States', 'China', 'Japan', 'Iran', 'Netherlands', 'Belgium']
 countries.sort()
 df_dict = {}
 
@@ -71,6 +87,6 @@ for country in df_dict.keys():
     y = list(df['total_deaths'])
     ax.plot(x, y, label=country)
 
-ax.legend()
+ax.legend(loc='lower right',prop={'size': 20}, frameon=True)
 
-fig.savefig('deaths_caused_by_coronavirus.svg')    
+fig.savefig('deaths_caused_by_coronavirus.png')    
