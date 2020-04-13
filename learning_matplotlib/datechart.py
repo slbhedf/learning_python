c5877216e5190e9731ce52e2920c3ab3ca5ad176
slbhedf <57523792+slbhedf@users.noreply.github.com>
2020-04-13 15:06:37 +0900

    Add files via upload

--- learning_matplotlib/datechart.py
+++ learning_matplotlib/datechart.py
@@ -3,21 +3,26 @@
 import pandas as pd
 from datetime import timedelta, datetime
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import numpy as np
 import os
 
+fontpath = 'C:\Windows\Fonts\msgothic.ttc'
+fontproperty = FontProperties(fname=fontpath)
+plt.rcParams['font.family'] = fontproperty.get_name()
+
 class DateChart(object):
-    def __init__(self, firstdate, enddate):
+    def __init__(self, firstdate, lastdate):
         self.fig = plt.figure(figsize=(15,15))
         self.ax = self.fig.subplots()
         self.set_firstdate(firstdate)
-        self.set_enddate(enddate)
+        self.set_lastdate(lastdate)
         
     def set_firstdate(self, d):
         self.firstdate = d
         
-    def set_enddate(self, d):
-        self.enddate = d
+    def set_lastdate(self, d):
+        self.lastdate = d
         
     def set_datelabels(self):
         xmajor_ticks = []
@@ -26,7 +31,7 @@ class DateChart(object):
         xminor_labels = []
         i = 0
         d = self.firstdate
-        while(d <= self.enddate):
+        while(d <= self.lastdate):
             if d.weekday() == 6:
                 xmajor_ticks.append(i)
                 xmajor_labels.append(d.strftime("%b %d"))
@@ -51,6 +56,7 @@ class DateChart(object):
         return x
     
     def set_logscale_yticks(self, maxdigits=5):
+        self.ax.set_yscale('log')
         k = 0
         ymajor_ticks = []
         yminor_ticks = []
@@ -70,12 +76,17 @@ class DateChart(object):
         self.ax.set_yticks(ticks=ymajor_ticks, minor=False)
         self.ax.set_yticks(ticks=yminor_ticks, minor=True)
         self.ax.set_yticklabels(ymajor_labels, minor=False)
-        self.ax.set_yticklabels(yminor_labels, minor=True)    
+        self.ax.set_yticklabels(yminor_labels, minor=True) 
+        
+
+class Covid19DataFrame():
+    pass
+    
 
 def covid19_worlddatechart():
     firstdate = datetime(2020,1,10)
-    enddate = datetime.today() - timedelta(days=1)
-    chart = DateChart(firstdate, enddate)
+    lastdate = datetime.today() - timedelta(days=1)
+    chart = DateChart(firstdate, lastdate)
     chart.set_datelabels()
     chart.ax.set_yscale('log')
     chart.ax.set_ylabel('total deaths', fontsize='25')
@@ -85,7 +96,7 @@ def covid19_worlddatechart():
         tick.label.set_fontsize(12) 
 
     df_all = pd.read_csv('full_data.csv', parse_dates=['date'])
-    df_all = df_all.loc[lambda df: df['date'] <= enddate,:]
+    df_all = df_all.loc[lambda df: df['date'] <= lastdate,:]
     
     countries = ['Italy', 'Spain','United Kingdom', 'Germany', 'France', 'United States', 'China', 'Iran']
     #countries = ['Italy', 'Spain','United Kingdom', 'Germany', 'France', 'United States', 'China', 'Japan', 'Iran', 'Netherlands', 'Belgium']
@@ -117,9 +128,63 @@ def covid19_worlddatechart():
     plt.show()
     chart.fig.savefig('svg/deaths_caused_by_coronavirus.svg')    
 
-def covid19_japanese_pref_datechart():
-    pass
 
+def covid19_japanese_pref_datechart():
+    datelabel = '確定日'
+    preflabel = '受診都道府県'
+    data = pd.read_csv('COVID-19.csv', parse_dates=[datelabel], encoding='utf8')
+    df = pd.DataFrame(data, columns=[datelabel, preflabel])
+    
+    firstdate = df[datelabel][0]
+    lastdate = datetime.today() - timedelta(days=3)
+    
+    df = df.loc[lambda df: df[datelabel] <= lastdate, :]
+    
+    prefs = sorted(list(set(df[preflabel])))
+    
+    cases_dict = {}
+    for i in df.index:
+        prefname = df[preflabel][i] 
+        d = df[datelabel][i] # Timestamp 
+        if cases_dict.get(prefname):
+            if cases_dict[prefname].get(d):
+                cases_dict[prefname][d] += 1
+            else:
+                cases_dict[prefname][d] = 1
+        else:
+            cases_dict[prefname] = {d: 1}
+    
+    chart = DateChart(firstdate, lastdate)        
+    for prefname in prefs:
+        d = list(cases_dict[prefname].keys())[0]
+        dates = []
+        y = []
+        totalcase = 0
+        while(d <= lastdate):
+            totalcase += cases_dict[prefname].get(d, 0)
+            y.append(totalcase)
+            dates.append(d)
+            d += timedelta(days=1)            
+        x = chart.dates_to_x(dates)
+        chart.ax.plot(x, y, '-',label=prefname)
+    
+    chart.set_datelabels()
+    chart.set_logscale_yticks(maxdigits=4)
+    chart.ax.legend()
+    
+    try:
+        os.mkdir('svg')
+    except OSError:
+        print("directory already exists")
+        
+    plt.tight_layout()
+    plt.show()
+    chart.fig.savefig('confirmed_cases_by_prefectures.svg')
+    
+    
 if __name__ == '__main__':
-    covid19_worlddatechart()
+#    covid19_worlddatechart()
+    covid19_japanese_pref_datechart()
+    
+    
     
